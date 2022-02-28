0a35fb4743143ab209560356115fc985f4a70257
User <abdurasilov99@gmail.com>
2022-02-28 01:38:44 -0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+ Week week = Week.Жума;
+ switch(week){
+     case Дуйшонбу -> System.out.println(" OOP ");
+     case Шейшенби -> System.out.println(" Exception ");
+     case Шаршенби -> System.out.println(" Enum ");
+     case Бейшенби -> System.out.println(" ToString ");
+     case Жума -> System.out.println(" ArrayList ");
+     case Ишенби -> System.out.println(" LinkedList ");
+     case Жекшенби -> System.out.println(" Set,Map ");
+
+ }
+    }
+}
